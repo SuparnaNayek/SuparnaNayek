28015b68c39b1a052d2d05ba3a3ad88c214be678
SuparnaNayek <100933093+SuparnaNayek@users.noreply.github.com>
2022-03-09 12:52:09 +0530

    Add files via upload

---
+++ Wednesday1/src/new1/TreeSetDemo.java
@@ -0,0 +1,18 @@
+package new1;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+TreeSet s=new TreeSet();
+s.add(5);
+s.add(3);
+s.add(20);
+s.add(7);
+s.add("World");
+s.add("Good Night");
+System.out.println(s);
+	}
+
+}
