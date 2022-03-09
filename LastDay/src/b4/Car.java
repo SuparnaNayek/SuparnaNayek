3818734971c2e1d08660d591c5a73d2bf9f934aa
SuparnaNayek <100933093+SuparnaNayek@users.noreply.github.com>
2022-03-09 12:59:49 +0530

    Add files via upload

---
+++ LastDay/src/b4/Car.java
@@ -0,0 +1,26 @@
+package b4;
+
+public class Car {
+	String color="pink";
+	int seats=4;
+	public class ConstructorDemo{
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car caraudi=new Car();
+Object o;
+o.color;
+Object audi;
+audi.color="black";
+System.out.println()
+
+
+
+
+
+	}
+
+}
