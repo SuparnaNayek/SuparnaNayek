20c57b4ec3186ec881838e64c8538342462ff21a
SuparnaNayek <100933093+SuparnaNayek@users.noreply.github.com>
2022-03-09 12:53:29 +0530

    Add files via upload

---
+++ Wednesday3/src/new3/TreeSet2.java
@@ -0,0 +1,16 @@
+package new3;
+import java.util.*;
+
+public class TreeSet2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+TreeSet<String>a1=new TreeSet<String>();
+a1.add("Ram");
+a1.add("Shyam");
+a1.add("Jadu");
+a1.add("Madhu");
+System.out.println(a1);
+	}
+
+}
