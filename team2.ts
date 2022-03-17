f1a3a30b6b0c472f526deab0213fe96b215a097b
SuparnaNayek <100933093+SuparnaNayek@users.noreply.github.com>
2022-03-17 16:43:19 +0530

    Add files via upload

---
+++ team2.ts
@@ -0,0 +1,9 @@
+class Person{
+firstName : String;
+lastName: String;
+
+};
+var objectPerson = new Person();
+objectPerson.firstName = "hulk";
+console.log(objectPerson.firstName);
+console.log(objectPerson.lastName);
