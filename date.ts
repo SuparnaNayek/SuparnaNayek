49fc07e47248fd6b92b25c35284e446a4ff15f45
SuparnaNayek <100933093+SuparnaNayek@users.noreply.github.com>
2022-03-18 06:41:38 +0530

    Add files via upload

---
+++ date.ts
@@ -0,0 +1,7 @@
+function dateTest(date:Date):void{
+	var hours=date.getHours();
+	var minutes=date.getSeconds();
+	console.log("Current Time:"+hours+":"+minutes+":"+seconds);
+	}
+	var date=new Date();
+}
