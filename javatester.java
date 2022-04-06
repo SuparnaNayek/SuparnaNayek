a0b9b2097f722ac8745f2bc7b52381c79b536956
SuparnaNayek <100933093+SuparnaNayek@users.noreply.github.com>
2022-04-06 09:32:09 +0530

    Add files via upload

---
+++ javatester.java
@@ -0,0 +1,26 @@
+package practicelink;
+import java.util.List;
+import java.util.ArrayList;
+
+public class javatester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+
+		   
+		      List names = new ArrayList();
+				
+		      names.add("Mahesh");
+		      names.add("Suresh");
+		      names.add("Ramesh");
+		      names.add("Naresh");
+		      names.add("Kalpesh");
+				
+		      names.forEach(System.out::println);
+		   }
+		}
+	
+
+
